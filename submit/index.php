2b7631da88e6350e0bbf57f6369a21edf1e71ed9
Jamie Lubiner <jamie@coverallcrew.com>
2016-02-10 22:32:17 -0800

    Spacing.

--- submit/index.php
+++ submit/index.php
@@ -95,11 +95,13 @@ error_reporting(0);
       <a href="https://vimeo.com/benaston" target="_blank"><img src="{{ 'jurors/ben_aston.png' | asset_path }}" width="80" height="80" alt="Ben Aston" /></a>
       <p>Ben is a 29-year-old, London born filmmaker, raised in Singapore, Hong Kong, Shanghai, Australia and Bath. His first stab at directing was a puppet show at the age of 11 that lasted so long as to require an intermission. It had dinosaurs. His no budget short ‘Russian Roulette’ played at Sundance 2015, going on to win the Jury Prize at last year's DFF. His LFS graduation film ‘He Took His Skin Off For Me’ premiered at the 2014 BFI London film festival and has gone on to viewed over 2 million times on Vimeo. As a result of this pretty gnarly year, Screen International named him a 'Star Of Tomorrow' in October 2015.</p>
 
+      <br>
       <h4>Business Club Royale</h4>
       <a href="https://www.businessclubroyale.com" target="_blank"><img src="{{ 'jurors/business_club_royale.png' | asset_path }}" width="80" height="80" alt="Business Club Royale" /></a>
       <p>Business Club Royale is a production company in Gothenburg on the west coast of Sweden. We strongly believe in exclusion. If something is intended for everyone, it ends up being for no one. We try not to communicate to boring people. Which is very liberating.
 So buckle up Jimmy, shit is about to get weird. Premium weird. We are: Christian Kuosmanen, Producer; Vedran Rupic, Director; Gustav Sundström, Director</p>
 
+      <br>
       <h4>William Caballero</h4>
       <a href="https://vimeo.com/cabal" target="_blank"><img src="{{ 'jurors/william_caballero.png' | asset_path }}" width="80" height="80" alt="William Caballero" /></a>
       <p>William D. Caballero is a multimedia filmmaker, composer, and video artist, specializing in telling big stories using small figures. His recent web­series Gran’pa Knows Best, which features 3D printed miniatures of his Puerto Rican grandfather, was licensed by HBO in a first­-time interstitial deal for the network. In addition, his autobiographical documentary American Dreams Deferred, and 3D scanned/printed inspirational series The Smallest Step, premiered on the World Channel in 2012 and 2015 respectively. Caballero is a recipient of the Gates MillenniumScholarship, and graduate of Pratt Institute and NYU.</p>
@@ -110,22 +112,27 @@ So buckle up Jimmy, shit is about to get weird. Premium weird. We are: Christian
       <img src="{{ 'jurors/daniel_kwan.png' | asset_path }}" width="80" height="80" alt="Daniel Kwan" />
       <p>DANIELS is comprised of two guys named Daniel [Daniel Scheinert and Daniel Kwan]. Daniel grew up in Massachusetts, began working in animation and graphics, when all of a sudden he met Daniel. Daniel is from Alabama and comes from a comedy and theatre background. Together they share the writing, directing, editing, and VFX on every kind of film they can possibly tackle. Often times they hold a camera. And when necessary they act, dance, or stunt double. They have a penchant for weird dramas and straight-faced comedies. Self-aware stories that tear themselves apart in an attempt to show audiences something a little bit new, or an attempt to be as honest as possible, or maybe just an attempt to not be boring. They’ve been nominated for a best music video Grammy, an MTV VMA award, awarded a UK MVA for Best Music Video 2011, and a Vimeo Award for Best Music Video in 2012. But most importantly they got into the 2014 Sundance Labs with their forthcoming film Swiss Army Man!  Collectively their music videos, short films, viral videos, commercials, have earned millions of views – and one time, they dressed up as pigeons and danced at the Hammer Museum. They both currently live and work in LA. But to be honest, Daniel does most of the work.</p>
 
+      <br>
       <h4>Clement Deneux</h4>
       <a href="https://vimeo.com/clementdeneux" target="_blank"><img src="{{ 'jurors/clement_deneux.png' | asset_path }}" width="80" height="80" alt="Clement Deneux" /></a>
       <p>Born in 1982 in France, Clement began his creative career by studying graphic design. In 2005, he began to design and produce awarded title sequences for different french channels. Between 2009 and 2011 he wrote and directed two short films: THE NAILS and ZOMBINLADEN: THE AXIS OF EVIL DEAD both of which went viral and were at the Disposable Film Festival. THE NAILS was awarded the Grand Prize at DFF2012. Clement is currently working on his feature film debut, DECOMPOSITION.</p>
 
+      <br>
       <h4>E.J. McLeavey-Fisher</h4>
       <a href="https://vimeo.com/ejmf" target="_blank"><img src="{{ 'jurors/ej_mcleavey.png' | asset_path }}" width="80" height="80" alt="E.J. McLeavey-Fisher" /></a>
       <p>E.J. McLeavey-Fisher is filmmaker based in New York City. He began his career at MTV, directing Emmy-nominated commercial, shortform, and longform content for the network.  Last year, his documentary short Comic Book Heaven was selected as a Vimeo Staff Pick and Short of the Week.  It has screened at film festivals around the world including the AFI DOCS, the Big Sky Documentary Film Festival, the Montclair Film Festival and the Disposable Film Festival, where it received an Honorable Mention.  His latest short The Dogist, released in early February also received a Staff Pick and has been featured on Short of the Week.</p>
 
+      <br>
       <h4>Bianca Giaever</h4>
       <a href="https://vimeo.com/biancagiaever" target="_blank"><img src="{{ 'jurors/bianca_giaever.png' | asset_path }}" width="80" height="80" alt="Bianca Giaever" /></a>
       <p>Bianca Giaever is a public radio producer and filmmaker. Her work has been featured in places such as This American Life, Radiolab, The New York Times, Buzzfeed, and the TED conference. She's interested in using nonfiction audio interviews to tell stories in new mediums.</p>
 
+      <br>
       <h4>Jamie Heinrich</h4>
       <a href="https://vimeo.com/heinrichfilm" target="_blank"><img src="{{ 'jurors/jamie_heinrich.png' | asset_path }}" width="80" height="80" alt="Jamie Heinrich" /></a>
       <p>Jamie is a DIY filmmaker residing in the greater Los Angeles area where he produces and directs film as part of a new breed of self-taught filmmakers. Jamie released some of the top snowboard and skateboard films worldwide before the age of 18 continuing through the 90's.  After making multiple short films he’s produced and directed 5 feature films in the last few years being distributed on major platforms where he’s won multiple festival awards.</p>
 
+      <br>
       <h4>Zac Hug</h4>
       <a href="https://vimeo.com/129702876" target="_blank"><img src="{{ 'jurors/zac_hug.png' | asset_path }}" width="80" height="80" alt="Zac Hug" /></a>
       <p>Zac Hug  is a Los Angeles based television writer (Drop Dead Diva), was previously the Director of Digital Content for ABC Family, and  was a talking head for So, Get This at Soapnet.com and Inside ABC Family. Shorts include All Kinds of Time and Yeux for Jamie Heinrich. Play productions include The Burden of Sunflowersfor the New York Fringe Festival and Williamstown Theatre Workshop, and Freaking Out for Breedingground Spring Fever.</p> 
@@ -135,6 +142,7 @@ So buckle up Jimmy, shit is about to get weird. Premium weird. We are: Christian
       <img src="{{ 'jurors/kirsten_lepore.png' | asset_path }}" width="80" height="80" alt="Kirsten Lepore" />
       <p>Kirsten Lepore is a Los Angeles-based director and animator, and alumna of CalArts. Her films have taken top prizes at SXSW, Slamdance, and many other international festivals. Commercially, she has worked with Google, MTV, Yo Gabba Gabba, Newsweek, Whole Foods, Toyota, Facebook, Nickelodeon, Nestle, and Glamour Magazine. Kirsten has given presentations everywhere from Pixar to Portugal and has also been featured in Juxtapoz, Shots, and was named one of the 50 most creative people by Creativity Magazine.</p>
 
+      <br>
       <h4>Anthony Sherin</h4>
       <a href="https://vimeo.com/user14700136" target="_blank"><img src="{{ 'jurors/anthony_sherin.png' | asset_path }}" width="80" height="80" alt="Anthony Sherin" /></a>
       <p>Anthony is an award-winning filmmaker whose short film, SOLO, PIANO – NYC, won the Audience Choice Award at the Disposable Film Festival 2013. It was also selected as one of the outstanding photo projects of today by Look3 Festival of the Photograph 2013 and was featured in the New York Times’ Op-Docs Series. His documentary, ORIGINAL INTENT: The Battle for America, aired on PBS. Anthony is also an accomplished film editor whose credits include ONE YEAR LEASE, best short documentary Tribeca Film Festival 2014, nominee for Cinema Eye Honors 2015, and official selection Sundance Film Festival 2015. His feature film credits include THE CURE (Universal), A SOLDIER’S SWEETHEART (Paramount/Showtime), and FIRST TIME FELON (HBO).</p>
